2bfd4403da93c25041cdc05ad23917f610024f25
Hasibul-Hasan01 <hasibul.outlook@gmail.com>
2025-11-18 23:50:54 +0600

    Footer Section Complete

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Outlet } from 'react-router';
 import Header from '../Components/Header/HEader';
 import Navbar from '../Shared/Navbar/Navbar';
+import Footer from '../Shared/Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
